69bfcf8f65240ce382a930987e0a17fb231e2d2d
nuilewis <lewisnuikweh@gmail.com>
2021-06-03 02:06:21 +0100

    Created directions_model.dart for direction functionality

---
+++ lib/Components/directions/directions_model.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Directions {
+  final LatLngBounds bounds;
+  final List<PointLatLng> polylinePoints;
+  final String totalDistance;
+  final String totalDuration;
+
+  const Directions({
+    @required this.bounds,
+    @required this.polylinePoints,
+    @required this.totalDistance,
+    @required this.totalDuration,
+  });
+  factory Directions.fromMap(Map<String, dynamic> map) {
+    //check if route is not available
+    if ((map['routes'] as List).isEmpty) {
+      print('routes list is empty');
+      return null;
+    }
+
+    // get route information
+    final data = Map<String, dynamic>.from(map['routes'][0]);
+
+    //bounds
+    final northeast = data['bounds']['northeast'];
+    final southwest = data['bounds']['southwest'];
+    final bounds = LatLngBounds(
+      southwest: LatLng(southwest['lat'], southwest['lng']),
+      northeast: LatLng(northeast['lat'], northeast['lng']),
+    );
+
+    ///Distance and Duration
+    String distance = '';
+    String duration = '';
+    if ((data['legs'] as List).isNotEmpty) {
+      final leg = data['legs'][0];
+      distance = leg['distance']['text'];
+      duration = leg['duration']['text'];
+    }
+
+    print('distance from direction model $distance');
+    print('duration from direction model $duration');
+    return Directions(
+      bounds: bounds,
+      polylinePoints:
+          PolylinePoints().decodePolyline(data['overview_polyline']['points']),
+      totalDistance: distance,
+      totalDuration: duration,
+    );
+  }
+}
