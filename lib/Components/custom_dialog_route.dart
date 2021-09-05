bf557827afb11941d62f9dc08bbc5680ae528f39
nuilewis <lewisnuikweh@gmail.com>
2021-09-05 20:26:53 +0100

    Adjusted stuff

--- lib/Components/custom_dialog_route.dart
+++ lib/Components/custom_dialog_route.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 ///{@template custom hero_dialod_route}
@@ -6,35 +5,39 @@ import 'package:flutter/material.dart';
 ///
 ///Best used with a [Hero] animation
 
-class  CustomHeroDialogRoute<T> extends PageRoute<T> {
+class CustomHeroDialogRoute<T> extends PageRoute<T> {
+  CustomHeroDialogRoute({
+    @required WidgetBuilder builder,
+    RouteSettings settings,
+    bool fullscreenDialog = false,
+  })  : _builder = builder,
+        super(settings: settings, fullscreenDialog: fullscreenDialog);
 
-CustomHeroDialogRoute({
-  @required WidgetBuilder builder,
-  RouteSettings settings,
-  bool fullscreenDialog = false,
-}) : _builder = builder, super(settings: settings, fullscreenDialog: fullscreenDialog);
+  final WidgetBuilder _builder;
 
-final WidgetBuilder _builder;
+  @override
+  bool get opaque => false;
+  @override
+  bool get barrierDismissible => true;
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
+  @override
+  bool get maintainState => true;
 
-@override
-bool get opaque => false;
-@override
-bool get barrierDismissible => true;
-@override 
-Duration get transitionDuration => const Duration(milliseconds:300);   
-@override 
-bool get maintainState=> true;
+  @override
+  Color get barrierColor => Colors.black54;
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    return child;
+  }
 
-@override 
-Color get barrierColor => Colors.black54;
-@override 
-Widget buildTransitions(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation, Widget child){ return child;}
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return _builder(context);
+  }
 
-@override 
-Widget buildPage(BuildContext context, Animation<double> animation, Animation<double> secondaryAnimation){
-  return _builder(context);
-}
-@override 
-String get barrierLabel => 'Popup dialog open';
-  
+  @override
+  String get barrierLabel => 'Popup dialog open';
 }
